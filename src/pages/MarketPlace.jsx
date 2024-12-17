26c2d0030b81e62b0745a7b926e799859202a3d3
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-12-17 13:19:16 +0200

    added filter card for the filter section

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,7 +1,12 @@
+import FilterCard from "../components/FilterCard";
+
 const MarketPlace = () => {
   return (
-    <div className="min-h-calc-100dvh-minus-80 lg:min-h-calc-100vh-minus-80 flex items-center justify-center">
-      MarketPlace
+    <div className="min-h-calc-100dvh-minus-80 lg:min-h-calc-100vh-minus-80 container mx-auto flex justify-center pt-20">
+      <FilterCard
+        img="https://static-00.iconduck.com/assets.00/code-tech-dev-icon-2048x1786-397qmt5y.png"
+        text="Full-Stack Development"
+      />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         "calc-100dvh-minus-80": "calc(100dvh - 80px)",
         "calc-100vh-minus-80": "calc(100vh - 80px)",
       },
+      boxShadow: {
+        xAndY: "0 0 5px  rgba(0, 0, 0, 0.2)",
+        hoverXandY: "0 0 5px  rgba(0, 0, 0, 0.3)",
+      },
     },
   },
   plugins: [],
