1e3b19288b34ea579b4338b4b0a1158070c743e5
NoorAthamny <athamnynoor@gmail.com>
2024-12-18 16:19:22 +0200

    introduction style

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,19 @@
 const Landing = () => {
   return (
-    <>
-      <article className="w-full border-[0.5rem] border-blue-700 p-1 text-center">
-        <p>
+    <main className="container mx-auto">
+      <article className="group relative mt-[50px] flex w-full flex-col items-center justify-center overflow-hidden rounded-xl border-blue-700 px-8 py-8 shadow-md">
+        <div className="absolute -bottom-20 -right-10 h-52 w-52 rounded-full bg-blue-400 opacity-50 blur-[50px] transition-all duration-300"></div>
+        <p className="w-3/5 text-center text-2xl">
           Peace Tech is a platform enabling diverse students to exchange
           services, earn in-app currency, and foster cross-cultural connections
           through rewarding collaborations.
         </p>
-        <button type="button">Explore</button>
+        <button
+          className="mt-4 rounded-xl bg-blue-500 px-6 py-1 text-2xl font-bold text-white"
+          type="button"
+        >
+          Explore
+        </button>
       </article>
 
       <article>
@@ -26,7 +32,7 @@ const Landing = () => {
           <button type="button">Explore</button>
         </section>
       </article>
-    </>
+    </main>
   );
 };
 
