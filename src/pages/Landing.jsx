f19d4fdeba861d9258933f950b73da1499e8d929
NoorAthamny <athamnynoor@gmail.com>
2024-12-18 15:40:49 +0200

    HomePage inial commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,32 @@
 const Landing = () => {
   return (
-    <div className="pagePadding flex min-h-calc-100dvh-minus-80 items-center justify-center lg:min-h-calc-100vh-minus-80">
-      Landing
-    </div>
+    <>
+      <article className="w-full border-[0.5rem] border-blue-700 p-1 text-center">
+        <p>
+          Peace Tech is a platform enabling diverse students to exchange
+          services, earn in-app currency, and foster cross-cultural connections
+          through rewarding collaborations.
+        </p>
+        <button type="button">Explore</button>
+      </article>
+
+      <article>
+        <section>
+          <img src="images\collaboration-icon.png" alt="collab-icon" />
+          <h3>Collaboration</h3>
+        </section>
+        <section>
+          <img src="images\cultural-icon.png" alt="cultural-icon" />
+          <h3>Cross-Cultural</h3>
+        </section>
+        <section>
+          <p>
+            Join Barter Exchange services, earn rewards, and connect cultures!
+          </p>
+          <button type="button">Explore</button>
+        </section>
+      </article>
+    </>
   );
 };
 
