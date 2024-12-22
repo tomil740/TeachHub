03e41f2a0650da8cc955884c0408292d7c59c707
NoorAthamny <athamnynoor@gmail.com>
2024-12-22 13:33:56 +0200

    loading cards

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -7,6 +7,10 @@ import Card from "./../components/card/Card";
 import categorizeUsers from "./../FirebaseFunctions/FetchFilteredData";
 
 const MarketPlace = () => {
+  // const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const [loading, setLoading] = useState(true);
+
   const [categories, setCategories] = useState({
     "Basic Programming": [],
     "Full-Stack Development": [],
@@ -25,12 +29,15 @@ const MarketPlace = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
+        setLoading(true);
         const categorizedData = await categorizeUsers();
         setCategories(categorizedData);
         const allUsers = Object.values(categorizedData).flat();
         setAllUsers(allUsers);
+        setLoading(false);
       } catch (error) {
         console.log("Error fetching and categorizing users:", error);
+        setLoading(false);
       }
     };
     fetchUsers();
@@ -44,17 +51,23 @@ const MarketPlace = () => {
 
       {/* Categories */}
 
-      <div className="flex flex-wrap gap-4">
-        {allUsers.map((user, userIndex) => (
-          <Card
-            key={userIndex}
-            name={user.name}
-            profession={user.profession}
-            description={user.bio}
-            image={user.imgUrl}
-          />
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex h-48 items-center justify-center">
+          <span>Loading...</span>{" "}
+        </div>
+      ) : (
+        <div className="flex flex-wrap gap-4">
+          {allUsers.map((user, userIndex) => (
+            <Card
+              key={userIndex}
+              name={user.name}
+              profession={user.profession}
+              description={user.bio}
+              image={user.imgUrl}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
